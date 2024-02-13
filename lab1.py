0bde3fe66bef425b5f8a2284df07f300f58c9f07
MZakicsn <159201590+MZakicsn@users.noreply.github.com>
2024-02-12 22:22:14 -0500

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+########   This script will do some addition and will print the results   #######
+
+# Two objects defined with num1 and num2
+num1 = 5
+num2 = 25
+
+# age object will add two numberical objects
+age = num1 + num2
+
+# To show the results of both objects and print the specifide name "isaac"
+print("Isaac is {} years old!".format(age))

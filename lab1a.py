04e7c73152987b10215a5cd122ae4fbdf7a92acf
Muhammad.Zikria <Mzikria@myseneca.ca>
2024-02-13 23:39:32 -0500

    modified code

--- lab1a.py
+++ lab1a.py
@@ -1,2 +1,14 @@
-#This file is created in branch lab1a
-#file name is lab1a.py
+#!/usr/bin/env python3
+
+# Prompt the user for "Name: "
+name = input('Name: ')
+
+# Prompt the user for "Age, accept only integers"
+try:
+    age = int(input('Age: '))
+except ValueError:
+    print("Please enter an integer for age.")
+    exit(1)  # Exit the program if the input is not an integer
+
+# Print the output
+print(f"Hi {name}, nice to meet you. You are {age} years old.")
